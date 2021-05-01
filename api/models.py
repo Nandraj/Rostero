9eca3e3d8c34548ee9bf6e2088fb1d8ce545bb35
Nandraj <nandraj.rathod.009@gmail.com>
2021-05-01 10:00:39 +0530

    category and product model added

--- api/models.py
+++ api/models.py
@@ -1,1 +1,19 @@
 from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=50, null=True)
+    price = models.FloatField()
+    opg_stock = models.FloatField()
+    # image =
+
+    category = models.ForeignKey(
+        Category, null=True, on_delete=models.SET_NULL)
