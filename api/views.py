06e5f5e4661646ee5bd117f68940e80f039e3368
Nandraj <nandraj.rathod.009@gmail.com>
2021-05-01 09:41:10 +0530

    url set for api/ and apiOverview added

--- api/views.py
+++ api/views.py
@@ -1,1 +1,8 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {}
+    return Response(api_urls)
