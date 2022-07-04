79cb221ba429ed8c3fbb1da57689e623443f64b4
Nandraj <nandraj.rathod.009@gmail.com>
2022-07-04 15:07:07 +0530

    setting - SECRET_KEY moved to untracked env_variables.py

--- _rostero/settings.py
+++ _rostero/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import env_variables
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pig1j+#ecx9nb)&l@s^g(s$um@33jp4__z-5v(x(&bi+ao26v9'
+SECRET_KEY = env_variables.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
